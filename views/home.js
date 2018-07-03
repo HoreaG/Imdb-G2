9be385ca567b18b2d874b133f2e52a922bc4ce46
vlladd112 <vlladd112@gmail.com>
2018-07-03 20:46:23 +0300

    photos added - vlad

--- views/home.js
+++ views/home.js
@@ -1,12 +1,24 @@
 window.onload = ()=>{
     const movieList = new MovieList();
     movieList.fetchDataList().then(()=>{
-      const container = document.getElementById('ceva');
+      const globalContainer = document.getElementById('ceva');
+      const moviesGlobalContainer = document.createElement('article');
+      moviesGlobalContainer.setAttribute('class', 'movies-global-container');
+      globalContainer.appendChild(moviesGlobalContainer);
       for( let i = 0; i < movieList.items.length; i++){
-          let movieElement = document.createElement('div');
-          //alert(movieList.items[i].Title);
-          movieElement.innerHTML = movieList.items[i].Title;
-          container.appendChild(movieElement);
+          const movieElement = document.createElement('div');
+          movieElement.setAttribute('class', 'movie-element');
+          moviesGlobalContainer.appendChild(movieElement);
+          const posterContainer = document.createElement('div');
+          posterContainer.setAttribute('class', 'poster-container');
+          movieElement.appendChild(posterContainer);
+          const moviePoster = document.createElement('img');
+          moviePoster.setAttribute('src',  movieList.items[i].Poster);
+          moviePoster.setAttribute('class', 'movie-poster');
+          posterContainer.appendChild(moviePoster);
+          const movieTitle  = document.createElement('p');
+          movieTitle.innerHTML = movieList.items[i].Title;
+          movieElement.appendChild(movieTitle);
       }
   }).catch(()=>{
       console.log('Eroare afisare');
