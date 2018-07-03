203e0075091c21bbcfbbcec5eb243baacb4f6c61
raziel0110 <albangabriel42@gmail.com>
2018-07-04 01:18:37 +0300

    details phase one

---
+++ views/movieDetails.js
@@ -0,0 +1,11 @@
+window. onload = ()=>{
+    const current_movie = new Movie();
+    const current_id = getUrlParameter('postId');
+}
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
