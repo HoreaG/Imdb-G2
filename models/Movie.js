508f2a8885f93fb11af0bc59cff2afe51b134bf2
raziel0110 <albangabriel42@gmail.com>
2018-07-05 10:25:32 +0300

    detail phase 3

--- models/Movie.js
+++ models/Movie.js
@@ -14,5 +14,26 @@ class Movie {
         this.Type = ""
     }
 
-   
+   MovieFetchData(id){
+       return $.ajax('https://ancient-caverns-16784.herokuapp.com/movies'+id,{
+           method : 'GET',
+           success : (data)=>{
+            this._id = data._id;
+            this.Title = data.Title;
+            this.Year = data.Year;
+            this.Runtime = data.Runtime;
+            this.Genre = data.Genre;
+            this.Language = data.Language;
+            this.Country = data.Country;
+            this.Poster = data.Poster;
+            this.imdbRating = data.imdbRating;
+            this.imdbVotes = data.imdbVotes;
+            this.imdbID = data.imdbID;
+            this.Type = data.Type;
+           },
+           error :()=>{
+               console.log('Error on showing the detail movie');
+           }
+       });
+   }
 }
