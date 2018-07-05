788b6f8a07826352c716ab5b8387ea486dc628cb
TorjaiT <torjai93@gmail.com>
2018-07-05 20:41:04 +0300

    register stuff, auth model

--- models/Auth.js
+++ models/Auth.js
@@ -1,1 +1,15 @@
-- - - - - - - - - 
+function Auth(){
+  this._id="";
+  this.username="";
+  this.password="";
+}
+
+Auth.prototype.createAuth=function (form){
+  console.log(form);
+    var url = "https://ancient-caverns-16784.herokuapp.com/auth/register";
+    var formData = {};
+    $(form).find("input[name]").each(function (index, node) {
+        formData[node.name] = node.value;
+    });
+    $.post(url, formData);
+}
